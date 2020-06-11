10f065dd28c54217a7c03033711a82df627b8f16
RedstoneParadox <spinjitzu00@gmail.com>
2020-06-10 19:39:20 -0700

    Mixin to try and override Minecraft

--- src/main/java/io/github/redstoneparadox/wayupthere/WayUpThere.kt
+++ src/main/java/io/github/redstoneparadox/wayupthere/WayUpThere.kt
@@ -1,9 +1,12 @@
 package io.github.redstoneparadox.wayupthere
 
+import io.github.redstoneparadox.wayupthere.world.SkyblockChunkGenerator
 import net.fabricmc.api.ModInitializer
+import net.minecraft.util.Identifier
+import net.minecraft.util.registry.Registry
 
 object WayUpThere: ModInitializer {
     override fun onInitialize() {
-
+        Registry.register(Registry.CHUNK_GENERATOR, Identifier("wayupthere", "skyblock"), SkyblockChunkGenerator.CODEC)
     }
 }
